bb5767fbbc007c79db63b1478d0f8f4170437304
rssa1230 <117625366+rssa1230@users.noreply.github.com>
2023-03-29 10:32:57 +0700

    Add files via upload

--- osmp.sql
+++ osmp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 19 Mar 2023 pada 07.47
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 8.1.6
+-- Generation Time: Mar 29, 2023 at 05:31 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `about`
+-- Table structure for table `about`
 --
 
 CREATE TABLE `about` (
@@ -44,7 +44,7 @@ CREATE TABLE `about` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `about`
+-- Dumping data for table `about`
 --
 
 INSERT INTO `about` (`id`, `about`, `visi`, `misi1`, `misi2`, `misi3`, `misi4`, `misi5`, `misi6`, `misi7`, `misi8`, `misi9`, `misi10`) VALUES
@@ -54,7 +54,7 @@ INSERT INTO `about` (`id`, `about`, `visi`, `misi1`, `misi2`, `misi3`, `misi4`, 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `agenda`
+-- Table structure for table `agenda`
 --
 
 CREATE TABLE `agenda` (
@@ -65,17 +65,17 @@ CREATE TABLE `agenda` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `agenda`
+-- Dumping data for table `agenda`
 --
 
 INSERT INTO `agenda` (`id`, `tanggal`, `agenda`, `ketuplak`) VALUES
-(4, '2023-05-03', 'PERPISAHAN KELAS 12', 'ROSSA NURAENI');
+(4, '2023-05-03', 'PERPISAHAN KELAS XII', 'ROSSA NURAENI');
 
 -- --------------------------------------------------------
 
 --
--- Stand-in struktur untuk tampilan `joinsiswa`
--- (Lihat di bawah untuk tampilan aktual)
+-- Stand-in structure for view `joinsiswa`
+-- (See below for the actual view)
 --
 CREATE TABLE `joinsiswa` (
 `id` int(9)
@@ -95,7 +95,7 @@ CREATE TABLE `joinsiswa` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jurusan`
+-- Table structure for table `jurusan`
 --
 
 CREATE TABLE `jurusan` (
@@ -105,7 +105,7 @@ CREATE TABLE `jurusan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `jurusan`
+-- Dumping data for table `jurusan`
 --
 
 INSERT INTO `jurusan` (`id`, `jurusan`, `keterangan`) VALUES
@@ -118,7 +118,7 @@ INSERT INTO `jurusan` (`id`, `jurusan`, `keterangan`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kelas`
+-- Table structure for table `kelas`
 --
 
 CREATE TABLE `kelas` (
@@ -127,7 +127,7 @@ CREATE TABLE `kelas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `kelas`
+-- Dumping data for table `kelas`
 --
 
 INSERT INTO `kelas` (`id`, `kelas`) VALUES
@@ -139,7 +139,7 @@ INSERT INTO `kelas` (`id`, `kelas`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `profil`
+-- Table structure for table `profil`
 --
 
 CREATE TABLE `profil` (
@@ -152,16 +152,16 @@ CREATE TABLE `profil` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `profil`
+-- Dumping data for table `profil`
 --
 
 INSERT INTO `profil` (`id`, `img`, `nama`, `ttl`, `alamat`, `site`) VALUES
-(1, 'rose.jpg', 'HILMI AHADIA', '2023-03-01', 'Tidak tahu', '@hilmi');
+(1, 'rose.jpg', 'MUHAMMAD HILMI AHADIAT', '2023-03-03', 'Tidak tahu', '@hilmi');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `proker`
+-- Table structure for table `proker`
 --
 
 CREATE TABLE `proker` (
@@ -171,22 +171,25 @@ CREATE TABLE `proker` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `proker`
+-- Dumping data for table `proker`
 --
 
 INSERT INTO `proker` (`id`, `jabatan`, `proker`) VALUES
-(3, 'KETUA OSIS', 'Memimpin organisasi dengan baik dan bijaksana; \r\nMengkoordinasikan semua aparat kepengurusan; \r\nMemimpin rapat; \r\nMenetapkan kebijaksanaan dan mengambil keputusan berdasarkan musyawarah dan mufakat; \r\nSetiap saat mengevaluasi kegiatan aparat kepengurusan.'),
-(4, 'WAKIL KETUA OSIS', 'Bersama-sama ketua menetapkan kebijaksanaan; \r\nMemberi saran kepada ketua dalam rangka mengambil keputusan; \r\nMenggantikan ketua jika berhalangan; \r\nMembantu ketua dalam melaksanakan tugas; \r\nBertanggung jawab kepada ketua.'),
+(3, 'KETUA', 'Memimpin organisasi dengan baik dan bijaksana; \r\nMengkoordinasikan semua aparat kepengurusan; \r\nMemimpin rapat; \r\nMenetapkan kebijaksanaan dan mengambil keputusan berdasarkan musyawarah dan mufakat; \r\nSetiap saat mengevaluasi kegiatan aparat kepengurusan.'),
+(4, 'WAKIL KETUA', 'Bersama-sama ketua menetapkan kebijaksanaan; \r\nMemberi saran kepada ketua dalam rangka mengambil keputusan; \r\nMenggantikan ketua jika berhalangan; \r\nMembantu ketua dalam melaksanakan tugas; \r\nBertanggung jawab kepada ketua.'),
 (5, 'SEKRETARIS', 'Memberi saran/ masukan kepada ketua dalam mengambil keputusan; \r\nBersama ketua menandatangani setiap surat; \r\nBertindak sebagai notulen dalam rapat, atau diserahkan kepada wakil sekretaris.'),
 (6, 'WAKIL SEKRETARIS', 'Menggantikan sekretaris jika sekretaris berhalangan.'),
-(7, 'BENDAHARA & WAKIL BENDAHARA', 'Bertanggung jawab dan mengEtahui segala pemasukan pengeluaran uang/biaya yang diperlukan; \r\nMembuat tanda bukti, setiap pemasukan/ pengeluaran uang untuk bertanggung jawab; \r\nBertanggung jawab atas inventaris dan pembendaharaan.'),
+(7, 'BENDAHARA & WAKIL BENDAHARA', 'Bertanggung jawab dan mengetahui segala pemasukan pengeluaran uang/biaya yang diperlukan; \r\nMembuat tanda bukti, setiap pemasukan/ pengeluaran uang untuk bertanggung jawab; \r\nBertanggung jawab atas inventaris dan pembendaharaan.'),
 (8, 'SEKBID 1', 'Mengkoordinir shalat dzuhur berjamaah;  Memperingati Maulid Nabi Muhammad; Melaksanakan lomba-lomba keagamaan; Infak jumat.'),
-(9, 'SEKBID 2', 'Tepat waktu datang ke sekolah; Memantau kedisiplinan; Memantau sosial media; Membuat jadwal jaga gerbang.');
+(9, 'SEKBID 2', 'Tepat waktu datang ke sekolah; Memantau kedisiplinan; Memantau sosial media; Membuat jadwal jaga gerbang.'),
+(10, 'KOMISI A', 'Membuat AD/ART (Anggaran Dasar/Anggaran Rumah Tangga) dan Kode Etik Siswa yang telah disusun dan ditetapkan bersama; \r\nMengawasi kinerja OSIS; \r\nMenginformasikan setiap permasalah AD/ART ke anggota MPK lain; \r\nMemberi saran/masukan kepada ketua dalam mengambil keputusan.'),
+(11, 'KOMISI B', 'Komisi B bertugas untuk membuat Anggaran Dasar dan Anggaran Rumah Tangga bersama Komisi A, ketua, dan beberapa orang terpilih; \r\nbersama dengan komisi A dan dewan harian menentukan matrix kerja MPK.'),
+(12, 'KOMISI C', 'Menampung dan menyalurkan aspirasi siswa kepa pihak sekolah; \r\nMelaksanakan fungsi pengawasan terhadap kinerja pengurus OSIS selama 1 tahun.');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `siswa`
+-- Table structure for table `siswa`
 --
 
 CREATE TABLE `siswa` (
@@ -203,16 +206,17 @@ CREATE TABLE `siswa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `siswa`
+-- Dumping data for table `siswa`
 --
 
 INSERT INTO `siswa` (`id`, `nis`, `foto`, `nama`, `tingkat`, `id_jurusan`, `id_kelas`, `keterangan`, `jabatan`, `hp`) VALUES
-(12, '2000', 'back1.png', 'ROSSA NURAENI', 'XI', 7, 6, 'OSIS', 'WAKIL KETUA OSIS', '088888');
+(1, '2000', 'mawar1.jpg', 'ROSSA NURAENI', 'XII', 5, 6, 'OSIS', 'WAKIL KETUA OSIS', '085703243781'),
+(2, '2022', 'back1.png', 'ROSE', 'XI', 5, 6, 'MPK', 'WAKIL KETUA MPK', '085703243781');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -224,7 +228,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `user`, `username`, `password`, `id_role`) VALUES
@@ -234,54 +238,54 @@ INSERT INTO `user` (`id`, `user`, `username`, `password`, `id_role`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur untuk view `joinsiswa`
+-- Structure for view `joinsiswa`
 --
 DROP TABLE IF EXISTS `joinsiswa`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `joinsiswa`  AS SELECT `a`.`id` AS `id`, `a`.`nis` AS `nis`, `a`.`foto` AS `foto`, `a`.`nama` AS `nama`, `a`.`tingkat` AS `tingkat`, `a`.`id_jurusan` AS `id_jurusan`, `b`.`jurusan` AS `jurusan`, `a`.`id_kelas` AS `id_kelas`, `c`.`kelas` AS `kelas`, `a`.`keterangan` AS `keterangan`, `a`.`jabatan` AS `jabatan`, `a`.`hp` AS `hp` FROM ((`siswa` `a` join `jurusan` `b` on(`a`.`id_jurusan` = `b`.`id`)) join `kelas` `c` on(`a`.`id_kelas` = `c`.`id`))  ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `joinsiswa`  AS SELECT `a`.`id` AS `id`, `a`.`nis` AS `nis`, `a`.`foto` AS `foto`, `a`.`nama` AS `nama`, `a`.`tingkat` AS `tingkat`, `a`.`id_jurusan` AS `id_jurusan`, `b`.`jurusan` AS `jurusan`, `a`.`id_kelas` AS `id_kelas`, `c`.`kelas` AS `kelas`, `a`.`keterangan` AS `keterangan`, `a`.`jabatan` AS `jabatan`, `a`.`hp` AS `hp` FROM ((`siswa` `a` join `jurusan` `b` on(`a`.`id_jurusan` = `b`.`id`)) join `kelas` `c` on(`a`.`id_kelas` = `c`.`id`)) ;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `about`
+-- Indexes for table `about`
 --
 ALTER TABLE `about`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `agenda`
+-- Indexes for table `agenda`
 --
 ALTER TABLE `agenda`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `jurusan`
+-- Indexes for table `jurusan`
 --
 ALTER TABLE `jurusan`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `kelas`
+-- Indexes for table `kelas`
 --
 ALTER TABLE `kelas`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `profil`
+-- Indexes for table `profil`
 --
 ALTER TABLE `profil`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `proker`
+-- Indexes for table `proker`
 --
 ALTER TABLE `proker`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `siswa`
+-- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
   ADD PRIMARY KEY (`id`),
@@ -290,69 +294,69 @@ ALTER TABLE `siswa`
   ADD KEY `id_kelas` (`id_kelas`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `about`
+-- AUTO_INCREMENT for table `about`
 --
 ALTER TABLE `about`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `agenda`
+-- AUTO_INCREMENT for table `agenda`
 --
 ALTER TABLE `agenda`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `jurusan`
+-- AUTO_INCREMENT for table `jurusan`
 --
 ALTER TABLE `jurusan`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT untuk tabel `kelas`
+-- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT untuk tabel `profil`
+-- AUTO_INCREMENT for table `profil`
 --
 ALTER TABLE `profil`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `proker`
+-- AUTO_INCREMENT for table `proker`
 --
 ALTER TABLE `proker`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT untuk tabel `siswa`
+-- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `siswa`
+-- Constraints for table `siswa`
 --
 ALTER TABLE `siswa`
   ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
